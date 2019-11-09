c3e95c85283be9534bcfe651f87014ba77e81e29
Joshua Jonas <mr.joshuajonas@gmail.com>
2019-11-08 20:14:49 -0800

    test

--- server.js
+++ server.js
@@ -28,31 +28,31 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
   .catch(err => console.log(err));
 
 // Passport middleware
-app.use(passport.initialize());
-// Passport config
-require("./config/passport")(passport);
+// app.use(passport.initialize());
+// // Passport config
+// require("./config/passport")(passport);
 
 ////
-app.use(function (req, res, next) {
-    // Website you wish to allow to
-    //either localhost:3000 or heroku deployed link (https://guarded-sands-13025.herokuapp.com)
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    // Request methods you wish to allow
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-    );
-    // Request headers you wish to allow
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "X-Requested-With,content-type"
-    );
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    // Pass to next layer of middleware
-    next();
-  });
+// app.use(function (req, res, next) {
+//     // Website you wish to allow to
+//     //either localhost:3000 or heroku deployed link (https://guarded-sands-13025.herokuapp.com)
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     // Request methods you wish to allow
+//     res.setHeader(
+//       "Access-Control-Allow-Methods",
+//       "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//     );
+//     // Request headers you wish to allow
+//     res.setHeader(
+//       "Access-Control-Allow-Headers",
+//       "X-Requested-With,content-type"
+//     );
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader("Access-Control-Allow-Credentials", true);
+//     // Pass to next layer of middleware
+//     next();
+//   });
 
 // Routes
 app.use("/api/users", users);
