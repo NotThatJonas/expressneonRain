4e458f9226cbccd3688a03a83939677e426a5098
Joshua Jonas <mr.joshuajonas@gmail.com>
2019-11-08 17:51:06 -0800

    cors

--- server.js
+++ server.js
@@ -28,8 +28,41 @@ mongoose
 app.use(passport.initialize());
 // Passport config
 require("./config/passport")(passport);
+
+////
+app.use(function (req, res, next) {
+    // Website you wish to allow to
+    //either localhost:3000 or heroku deployed link (https://guarded-sands-13025.herokuapp.com)
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    // Request methods you wish to allow
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    // Request headers you wish to allow
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    // Pass to next layer of middleware
+    next();
+  });
+  
 // Routes
 app.use("/api/users", users);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+
+
+///////////
+
+
+
+
+
+
+
