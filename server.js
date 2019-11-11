2e3c2d32cebf82ed9bb8ba139c03f4b56e5778b0
Joshua Jonas <mr.joshuajonas@gmail.com>
2019-11-11 15:36:45 -0800

    reverting to before slash in cors

--- server.js
+++ server.js
@@ -6,24 +6,6 @@ const passport = require("passport");
 const users = require("./routes/api/users");
 
 const app = express();
-const cookieParser = require('cookie-parser');
-const session = require('express-session')
-const MongoStore = require('connect-mongo')(session);
-
-// mongoose.connect('mongodb://localhost/my-database', {
-//     useMongoClient: true
-// });
-// mongoose.Promise = global.Promise;
-// const db = mongoose.connection
-
-// app.use(cookieParser());
-// app.use(session({
-//     secret: 'my-secret',
-//     resave: false,
-//     saveUninitialized: true,
-//     store: new MongoStore({ mongooseConnection: db })
-// }));
-
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
@@ -37,6 +19,11 @@ app.use(bodyParser.json());
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/neonraindb'
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
 // Connect to MongoDB
+// mongoose
+//   .connect(
+//     db,
+//     { useNewUrlParser: true }
+//   )
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
 
@@ -49,7 +36,7 @@ require("./config/passport")(passport);
 app.use(function (req, res, next) {
     // Website you wish to allow to
     //either localhost:3000 or heroku deployed link (https://guarded-sands-13025.herokuapp.com)
-    res.setHeader("Access-Control-Allow-Origin", "https://neonraingame.herokuapp.com"); //https://neonraingame.herokuapp.com / https://neonrain.firebaseapp.com
+    res.setHeader("Access-Control-Allow-Origin", "https://neonrain.firebaseapp.com"); //https://neonraingame.herokuapp.com
     // Request methods you wish to allow
     res.setHeader(
       "Access-Control-Allow-Methods",
@@ -72,6 +59,104 @@ app.use("/api/users", users);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+
+
+///////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const bodyParser = require("body-parser");
+
+// const passport = require("passport");
+// const users = require("./routes/api/users");
+
+// const app = express();
+// const cookieParser = require('cookie-parser');
+// const session = require('express-session')
+// const MongoStore = require('connect-mongo')(session);
+
+// // mongoose.connect('mongodb://localhost/my-database', {
+// //     useMongoClient: true
+// // });
+// // mongoose.Promise = global.Promise;
+// // const db = mongoose.connection
+
+// // app.use(cookieParser());
+// // app.use(session({
+// //     secret: 'my-secret',
+// //     resave: false,
+// //     saveUninitialized: true,
+// //     store: new MongoStore({ mongooseConnection: db })
+// // }));
+
+// // Bodyparser middleware
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: false
+//   })
+// );
+// app.use(bodyParser.json());
+// // DB Config
+// // const db = require("./congfig/keys");
+
+// const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/neonraindb'
+// mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+// // Connect to MongoDB
+//   .then(() => console.log("MongoDB successfully connected"))
+//   .catch(err => console.log(err));
+
+// // Passport middleware
+// app.use(passport.initialize());
+// // Passport config
+// require("./config/passport")(passport);
+
+// ////
+// app.use(function (req, res, next) {
+//     // Website you wish to allow to
+//     //either localhost:3000 or heroku deployed link (https://guarded-sands-13025.herokuapp.com)
+//     res.setHeader("Access-Control-Allow-Origin", "https://neonraingame.herokuapp.com"); //https://neonraingame.herokuapp.com / https://neonrain.firebaseapp.com
+//     // Request methods you wish to allow
+//     res.setHeader(
+//       "Access-Control-Allow-Methods",
+//       "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//     );
+//     // Request headers you wish to allow
+//     res.setHeader(
+//       "Access-Control-Allow-Headers",
+//       "X-Requested-With,content-type"
+//     );
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader("Access-Control-Allow-Credentials", true);
+//     // Pass to next layer of middleware
+//     next();
+//   });
+
+// // Routes
+// app.use("/api/users", users);
+
+// const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+// app.listen(port, () => console.log(`Server up and running on port ${port} !`));
 
 
 ///////////
